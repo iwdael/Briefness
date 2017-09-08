7e95a4119dac765d16a5afc7e33abc575ce70ede
mr-absurd <4884280@qq.com>
2017-09-08 09:59:48 +0800

    Update Briefness.java

--- libsbriefness/src/main/java/com/absurd/briefness/Briefness.java
+++ libsbriefness/src/main/java/com/absurd/briefness/Briefness.java
@@ -13,9 +13,7 @@ import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.HashMap; 
 
 /**
  * Author: mr-absurd
@@ -27,8 +25,8 @@ public class Briefness {
 
     public static void bind(Context context) {
         bindLayout(context);
-        bindViews(context);
         bindView(context);
+        bindViews(context);
         bindClick(context);
     }
 
@@ -39,178 +37,233 @@ public class Briefness {
     }
 
     private static void bindClick(Object context, View view) {
+        Class clazz = context.getClass();
+        Method[] methods = clazz.getMethods();
+        Method[] var4 = methods;
+        int var5 = methods.length;
 
+        for(int var6 = 0; var6 < var5; ++var6) {
+            Method method = var4[var6];
+            BindClick bindClick = (BindClick)method.getAnnotation(BindClick.class);
+            if(bindClick != null) {
+                EventBus eventBus = (EventBus)bindClick.annotationType().getAnnotation(EventBus.class);
+                if(eventBus != null) {
+                    String listener = eventBus.listenerSetter();
+                    String callBack = eventBus.callBackMethod();
+                    Class listenerType = eventBus.listenerType();
+                    HashMap hashMap = new HashMap();
+                    hashMap.put(callBack, method);
+                    int[] resIds = bindClick.value();
 
-        Class<?> clazz = context.getClass();
-        Method[] methods = clazz.getDeclaredMethods();
-        for (Method method : methods) {
-            BindClick bindClick = method.getAnnotation(BindClick.class);
-            if (bindClick == null) continue;
-            EventBus eventBus = bindClick.annotationType().getAnnotation(EventBus.class);
-            if (eventBus == null) {
-                continue;
-            }
-            String listener = eventBus.listenerSetter();
-            String callBack = eventBus.callBackMethod();
-            Class<?> listenerType = eventBus.listenerType();
-            Map<String, Method> hashMap = new HashMap<>();
-            hashMap.put(callBack, method);
-            int[] resIds = bindClick.value();
-            try {
-                for (int resId : resIds) {
-                    Method findViewById = view.getClass().getMethod("findViewById", int.class);
-                    View view1 = (View) findViewById.invoke(view, resId);
-                    if (view1 == null) continue;
-                    Method setLinstener = view1.getClass().getMethod(listener, listenerType);
-                    ListenerInvocationHandler handler = new ListenerInvocationHandler(context, hashMap);
-                    Object proxy = Proxy.newProxyInstance(view.getContext().getClassLoader(), new Class[]{listenerType}, handler);
-                    setLinstener.invoke(view1, proxy);
+                    try {
+                        int[] e = resIds;
+                        int var16 = resIds.length;
+
+                        for(int var17 = 0; var17 < var16; ++var17) {
+                            int resId = e[var17];
+                            Method findViewById = view.getClass().getMethod("findViewById", new Class[]{Integer.TYPE});
+                            View view1 = (View)findViewById.invoke(view, new Object[]{Integer.valueOf(resId)});
+                            if(view1 != null) {
+                                Method setLinstener = view1.getClass().getMethod(listener, new Class[]{listenerType});
+                                ListenerInvocationHandler handler = new ListenerInvocationHandler(context, hashMap);
+                                Object proxy = Proxy.newProxyInstance(view.getContext().getClassLoader(), new Class[]{listenerType}, handler);
+                                setLinstener.invoke(view1, new Object[]{proxy});
+                            }
+                        }
+                    } catch (Exception var24) {
+                        var24.printStackTrace();
+                    }
                 }
-            } catch (Exception e) {
-                e.printStackTrace();
             }
-
         }
 
     }
 
     private static void bindViews(Object context, View view) {
-        Class<?> clazz = context.getClass();
-        Class<?> viewClazz = view.getClass();
+        Class clazz = context.getClass();
+        Class viewClazz = view.getClass();
         Field[] fields = clazz.getFields();
-        for (Field field : fields) {
-            BindViews bindView = field.getAnnotation(BindViews.class);
-            if (bindView == null) continue;
-            int[] resIds = bindView.value();
-            List<View> list = new ArrayList<>();
-            try {
-                for (int resId : resIds) {
-                    Method findViewById = viewClazz.getMethod("findViewById", int.class);
-                    View view1 = (View) findViewById.invoke(view, resId);
-                    list.add(view1);
+        Field[] var5 = fields;
+        int var6 = fields.length;
+
+        for(int var7 = 0; var7 < var6; ++var7) {
+            Field field = var5[var7];
+            BindViews bindView = (BindViews)field.getAnnotation(BindViews.class);
+            if(bindView != null) {
+                int[] resIds = bindView.value();
+                ArrayList list = new ArrayList();
+
+                try {
+                    int[] e = resIds;
+                    int var13 = resIds.length;
+
+                    for(int var14 = 0; var14 < var13; ++var14) {
+                        int resId = e[var14];
+                        Method findViewById = viewClazz.getMethod("findViewById", new Class[]{Integer.TYPE});
+                        View view1 = (View)findViewById.invoke(view, new Object[]{Integer.valueOf(resId)});
+                        list.add(view1);
+                    }
+                } catch (Exception var19) {
+                    var19.printStackTrace();
                 }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            field.setAccessible(true);
-            try {
-                field.set(context, list);
-            } catch (IllegalAccessException e) {
-                e.printStackTrace();
+
+                field.setAccessible(true);
+
+                try {
+                    field.set(context, list);
+                } catch (IllegalAccessException var18) {
+                    var18.printStackTrace();
+                }
             }
         }
-
 
     }
 
     private static void bindView(Object context, View view) {
-        Class<?> clazz = context.getClass();
+        Class clazz = context.getClass();
         Field[] fields = clazz.getFields();
-        for (Field field : fields) {
-            BindView bindView = field.getAnnotation(BindView.class);
-            if (bindView == null) continue;
-            int resId = bindView.value();
-            try {
-                Method findViewById = view.getClass().getMethod("findViewById", int.class);
-                View view1 = (View) findViewById.invoke(view, resId);
-                field.setAccessible(true);
-                field.set(context, view1);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+        Field[] var4 = fields;
+        int var5 = fields.length;
 
+        for(int var6 = 0; var6 < var5; ++var6) {
+            Field field = var4[var6];
+            BindView bindView = (BindView)field.getAnnotation(BindView.class);
+            if(bindView != null) {
+                int resId = bindView.value();
+
+                try {
+                    Method e = view.getClass().getMethod("findViewById", new Class[]{Integer.TYPE});
+                    View view1 = (View)e.invoke(view, new Object[]{Integer.valueOf(resId)});
+                    field.setAccessible(true);
+                    field.set(context, view1);
+                } catch (Exception var12) {
+                    var12.printStackTrace();
+                }
+            }
         }
+
     }
 
     private static void bindLayout(Context context) {
-        Class<?> clazz = context.getClass();
-        BindLayout bindLayout = clazz.getAnnotation(BindLayout.class);
-        if (bindLayout == null) return;
-        int resId = bindLayout.value();
-        try {
-            Method setContentView = clazz.getMethod("setContentView", int.class);
-            setContentView.invoke(context, resId);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        Class clazz = context.getClass();
+        BindLayout bindLayout = (BindLayout)clazz.getAnnotation(BindLayout.class);
+        if(bindLayout != null) {
+            int resId = bindLayout.value();
 
+            try {
+                Method e = clazz.getMethod("setContentView", new Class[]{Integer.TYPE});
+                e.invoke(context, new Object[]{Integer.valueOf(resId)});
+            } catch (Exception var5) {
+                var5.printStackTrace();
+            }
+
+        }
     }
 
     private static void bindView(Context context) {
-        Class<?> clazz = context.getClass();
-        Field[] fields = clazz.getDeclaredFields();
-        for (Field field : fields) {
-            BindView bindView = field.getAnnotation(BindView.class);
-            if (bindView == null) continue;
-            int resId = bindView.value();
-            try {
-                Method findViewById = clazz.getMethod("findViewById", int.class);
-                View view = (View) findViewById.invoke(context, resId);
-                field.setAccessible(true);
-                field.set(context, view);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+        Class clazz = context.getClass();
+        Field[] fields = clazz.getFields();
+        Field[] var3 = fields;
+        int var4 = fields.length;
 
+        for(int var5 = 0; var5 < var4; ++var5) {
+            Field field = var3[var5];
+            BindView bindView = (BindView)field.getAnnotation(BindView.class);
+            if(bindView != null) {
+                int resId = bindView.value();
+
+                try {
+                    Method e = clazz.getMethod("findViewById", new Class[]{Integer.TYPE});
+                    View view = (View)e.invoke(context, new Object[]{Integer.valueOf(resId)});
+                    field.setAccessible(true);
+                    field.set(context, view);
+                } catch (Exception var11) {
+                    var11.printStackTrace();
+                }
+            }
         }
+
     }
 
     private static void bindViews(Context context) {
-        Class<?> clazz = context.getClass();
-        Field[] fields = clazz.getDeclaredFields();
-        for (Field field : fields) {
-            BindViews bindView = field.getAnnotation(BindViews.class);
-            if (bindView == null) continue;
-            int[] resIds = bindView.value();
-            List<View> list = new ArrayList<>();
-            try {
-                for (int resId : resIds) {
-                    Method findViewById = clazz.getMethod("findViewById", int.class);
-                    View view = (View) findViewById.invoke(context, resId);
-                    list.add(view);
+        Class clazz = context.getClass();
+        Field[] fields = clazz.getFields();
+        Field[] var3 = fields;
+        int var4 = fields.length;
+
+        for(int var5 = 0; var5 < var4; ++var5) {
+            Field field = var3[var5];
+            BindViews bindView = (BindViews)field.getAnnotation(BindViews.class);
+            if(bindView != null) {
+                int[] resIds = bindView.value();
+                ArrayList list = new ArrayList();
+
+                try {
+                    int[] e = resIds;
+                    int var11 = resIds.length;
+
+                    for(int var12 = 0; var12 < var11; ++var12) {
+                        int resId = e[var12];
+                        Method findViewById = clazz.getMethod("findViewById", new Class[]{Integer.TYPE});
+                        View view = (View)findViewById.invoke(context, new Object[]{Integer.valueOf(resId)});
+                        list.add(view);
+                    }
+                } catch (Exception var17) {
+                    var17.printStackTrace();
                 }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            field.setAccessible(true);
-            try {
-                field.set(context, list);
-            } catch (IllegalAccessException e) {
-                e.printStackTrace();
+
+                field.setAccessible(true);
+
+                try {
+                    field.set(context, list);
+                } catch (IllegalAccessException var16) {
+                    var16.printStackTrace();
+                }
             }
         }
+
     }
 
     private static void bindClick(Context context) {
-        Class<?> clazz = context.getClass();
-        Method[] methods = clazz.getDeclaredMethods();
-        for (Method method : methods) {
-            BindClick bindClick = method.getAnnotation(BindClick.class);
-            if (bindClick == null) continue;
-            EventBus eventBus = bindClick.annotationType().getAnnotation(EventBus.class);
-            if (eventBus == null) {
-                continue;
-            }
-            String listener = eventBus.listenerSetter();
-            String callBack = eventBus.callBackMethod();
-            Class<?> listenerType = eventBus.listenerType();
-            Map<String, Method> hashMap = new HashMap<>();
-            hashMap.put(callBack, method);
-            int[] resIds = bindClick.value();
-            try {
-                for (int resId : resIds) {
-                    Method findViewById = clazz.getMethod("findViewById", int.class);
-                    View view = (View) findViewById.invoke(context, resId);
-                    if (view == null) continue;
-                    Method setLinstener = view.getClass().getMethod(listener, listenerType);
-                    ListenerInvocationHandler handler = new ListenerInvocationHandler(context, hashMap);
-                    Object proxy = Proxy.newProxyInstance(context.getClassLoader(), new Class[]{listenerType}, handler);
-                    setLinstener.invoke(view, proxy);
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+        Class clazz = context.getClass();
+        Method[] methods = clazz.getMethods();
+        Method[] var3 = methods;
+        int var4 = methods.length;
 
+        for(int var5 = 0; var5 < var4; ++var5) {
+            Method method = var3[var5];
+            BindClick bindClick = (BindClick)method.getAnnotation(BindClick.class);
+            if(bindClick != null) {
+                EventBus eventBus = (EventBus)bindClick.annotationType().getAnnotation(EventBus.class);
+                if(eventBus != null) {
+                    String listener = eventBus.listenerSetter();
+                    String callBack = eventBus.callBackMethod();
+                    Class listenerType = eventBus.listenerType();
+                    HashMap hashMap = new HashMap();
+                    hashMap.put(callBack, method);
+                    int[] resIds = bindClick.value();
+
+                    try {
+                        int[] e = resIds;
+                        int var15 = resIds.length;
+
+                        for(int var16 = 0; var16 < var15; ++var16) {
+                            int resId = e[var16];
+                            Method findViewById = clazz.getMethod("findViewById", new Class[]{Integer.TYPE});
+                            View view = (View)findViewById.invoke(context, new Object[]{Integer.valueOf(resId)});
+                            if(view != null) {
+                                Method setLinstener = view.getClass().getMethod(listener, new Class[]{listenerType});
+                                ListenerInvocationHandler handler = new ListenerInvocationHandler(context, hashMap);
+                                Object proxy = Proxy.newProxyInstance(context.getClassLoader(), new Class[]{listenerType}, handler);
+                                setLinstener.invoke(view, new Object[]{proxy});
+                            }
+                        }
+                    } catch (Exception var23) {
+                        var23.printStackTrace();
+                    }
+                }
+            }
         }
+
     }
 }
